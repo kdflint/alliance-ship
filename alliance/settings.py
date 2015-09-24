3f44d7b3ce765d768caa4b6105ef99094997b2a9
Kathy Flint <kathy.flint@northbridgetech.org>
2015-09-24 15:33:16 -0500

    heroku

--- alliance/settings.py
+++ alliance/settings.py
@@ -86,6 +86,13 @@ SESSION_COOKIE_AGE = 10 * 60  # 10 minutes
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
