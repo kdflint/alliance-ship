0e6805f5ee741e8e933a1dfef52be732bc76abff
Kathy Flint <kathy.flint@northbridgetech.org>
2015-10-19 20:12:33 -0500

    baby steps

--- alliance/config/settings/base.py
+++ alliance/config/settings/base.py
@@ -60,12 +60,12 @@ EMAIL_PORT = os.getenv('ALLIANCE_SMTP_PORT')
 EMAIL_RECIPIENT_LIST = os.getenv('ALLIANCE_EMAIL_HOST_USER')
 EMAIL_SUBJECT_PREFIX = '[%s]' % PROJECT_NAME
 
-LOGGING_CONFIG = None
+#LOGGING_CONFIG = None
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 CORE_PROJECT_DIR = os.path.join(BASE_DIR, 'alliance/core')
 
-logging.config.fileConfig(os.path.join(CORE_PROJECT_DIR, 'logging.ini'))
+#logging.config.fileConfig(os.path.join(CORE_PROJECT_DIR, 'logging.ini'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
