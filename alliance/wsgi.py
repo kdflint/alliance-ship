7e3d43e2d10b069ddb536feb4948e2d760c1d5be
Kathy Flint <kathy.flint@northbridgetech.org>
2015-10-20 16:24:28 -0500

    baby steps

--- alliance/wsgi.py
+++ alliance/wsgi.py
@@ -11,6 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application  
 
+# This accomodates gunicorn pathing on Heroku
+# http://stackoverflow.com/questions/11660627/python-app-import-error-in-django-with-wsgi-gunicorn
+sys.path.insert(1, os.path.dirname(os.path.realpath(__file__)))
+
 #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alliance.settings")   
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alliance.config.settings.base")   
 
