fc2a599f5a5c3a7c4c5ecbf9251d224c0f0d6a10
Kathy Flint <kathy.flint@northbridgetech.org>
2015-10-21 12:07:19 -0500

    baby steps

--- alliance/config/settings/dev.py
+++ alliance/config/settings/dev.py
@@ -1,10 +1,14 @@
+#############################################################################################################
+# This settings file is purposed for our centralized dev environment installed at alliance-dev.herokuapp.com
+# If these configurations do not suit your environment, use a local.py settings file 
+# Do not modify these configurations to suit any environment except the one at alliance-dev.herokuapp.com
+#############################################################################################################
+
 # Import base to override settings
 from .base import *
 import os
 
-# Override Github settings
 DEBUG = os.getenv('DEBUG', True)
 
 if os.path.isfile('local.py'):
     from .local import *
-
