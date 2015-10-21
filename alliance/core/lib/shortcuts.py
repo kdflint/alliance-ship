5e25af282221287eaa97a0a1c9a5866adbd6db55
Kathy Flint <kathy.flint@northbridgetech.org>
2015-10-21 18:50:13 -0500

    global name body is not defined

--- alliance/core/lib/shortcuts.py
+++ alliance/core/lib/shortcuts.py
@@ -8,7 +8,7 @@ logger = logging.getLogger("playbook")
 def create_json_message_object(message, field='__all__', code='invalid'):
     return json.dumps({field: [{'message': message, 'code': code}]})
 
-def send_email(subject, to=settings.EMAIL_RECIPIENT_LIST):
+def send_email(subject, body='test', to=settings.EMAIL_RECIPIENT_LIST):
     try:
         send_mail(subject, body, EMAIL_HOST_USER, to, fail_silently=False)
     except:
