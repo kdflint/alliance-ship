f5984638dada3be7676a0f573ebbddb714664bd3
Kathy Flint <kathy.flint@northbridgetech.org>
2015-10-21 11:18:38 -0500

    baby steps

--- alliance/email_settings.py
+++
@@ -1,7 +0,0 @@
-# Email configuration
-#EMAIL_USE_TLS = True
-#EMAIL_HOST = 'smtp.gmail.com'
-#EMAIL_HOST_USER = 'kathy.flint@northbridgetech.org'
-#EMAIL_HOST_PASSWORD = 'LjPPk995!'
-#EMAIL_PORT = 587
-#EMAIL_RECIPIENT_LIST = [EMAIL_HOST_USER]
