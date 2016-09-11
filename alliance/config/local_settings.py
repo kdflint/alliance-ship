257b3fdc5a53b64258a82371790e2be090049db3
Kathy Flint <kathy.flint@northbridgetech.org>
2016-09-11 10:52:59 -0500

    added local settings

--- alliance/config/local_settings.py
+++ alliance/config/local_settings.py
@@ -1,12 +1,60 @@
-DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.postgresql_psycopg2',
-		'NAME': 'northbr6_devwaterwheel',
-		'USER': 'alliance',
-		'PASSWORD': 'beloved',
-		'HOST': '127.0.0.1',
-		'PORT': '5432',
+import dj_database_url
+import os
+import logging.config
+from os.path import abspath, basename, dirname, join, normpath
+from os import listdir
+from sys import path
+
+SESSION_COOKIE_SECURE = os.getenv('ALLIANCE_SESSION_COOKIE_SECURE', 'False')
+
+SESSION_COOKIE_SECURE = 'True'
+SESSION_COOKIE_HTTPONLY = 'True'
+CSRF_COOKIE_SECURE = 'True'
+CSRF_COOKIE_HTTPONLY = 'True'
+SOCIAL_AUTH_REDIRECT_IS_HTTPS = 'True'
+LOGIN_ERROR_URL = '/logged/'
+
+GITHUB_SOCIAL_AUTH_RAISE_EXCEPTIONS = 'True'
+SOCIAL_AUTH_RAISE_EXCEPTIONS = 'True'
+RAISE_EXCEPTIONS = 'True'
+DEBUG = 'True'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            #'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+            #'stream': 'sys.stdout'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers':['console'],
+            'propagate': True,
+            'level':'DEBUG',
+        },
+        'alliance': {
+            'handlers': ['console'],
+            'propagate': True,
+            'level': 'DEBUG',
+        },
     }
 }
 
-SECRET_KEY = '-ccj-m$@5h9z$t%+9zq6z$y@s%e9+kapdy^ozt4k^lfvpq)bxm'
+
