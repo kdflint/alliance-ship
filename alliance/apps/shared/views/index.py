6bbb1acc660152772ea080e4f81e9a509435cfaa
Kathy Flint <kathy.flint@northbridgetech.org>
2016-09-11 20:52:50 -0500

    oops on last commit

--- alliance/apps/shared/views/index.py
+++ alliance/apps/shared/views/index.py
@@ -44,7 +44,7 @@ def index(request):
         if (len(teams) == 0):
             request.session['team'] = None
         #elif (len(teams) == 1):
-        elif (len(teams) > 1):
+        elif (len(teams) > 0):
             request.session['team'] = teams[0].id
             request.session['team'] = 1
         else:
