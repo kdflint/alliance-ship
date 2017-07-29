6b80d84e5c7f4e6e93e8ac81a1f57ee124117309
Kathy Flint <kathy.flint@northbridgetech.org>
2017-07-28 20:03:24 -0500

    #80; removed unused code; added some comments

--- alliance/config/local_settings_template.py
+++ alliance/config/local_settings_template.py
@@ -1,7 +1,3 @@
-# This file is only a pattern. It is not referenced at runtime.
-# It is used during vagrant provisioning to create a local, unsourced settings file,
-# which should then be updated with correct values according to install instructions.
-
 DATABASES = {
 	'default': {
 		'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -13,13 +9,11 @@ DATABASES = {
     }
 }
 
-# TODO - what is this used for and where is it from??
+# TODO - what is this used for and where is it from?
 SECRET_KEY = '-ccj-m$@5h9z$t%+9zq6z$y@s%e9+kapdy^ozt4k^lfvpq)abc'
 
-# Update value to match GitHub OAuth applications: Client Id
 # This credentials OAuth conversation
 SOCIAL_AUTH_GITHUB_KEY = 'd65f4ff7e0cddcc12345'
 
-# Update value to match GitHub OAuth applications: Client Secret
 # This credentials OAuth conversation
 SOCIAL_AUTH_GITHUB_SECRET = '6fbe9f90498881c31a0d0f1fa2512430a7c12345'
